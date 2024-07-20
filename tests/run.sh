088a0f4c00405067bedc8d6086b256cf4ec07f2e
MoneroOcean <support@moneroocean.stream>
2024-07-20 05:18:49 +0300

    Added SAL support

--- tests/run.sh
+++ tests/run.sh
@@ -3,4 +3,16 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 cd $DIR
-node sal.js || exit 1
+node bloc.js || exit 1
+node ird.js  || exit 1
+node msr.js  || exit 1
+node ryo.js  || exit 1
+node sal.js  || exit 1
+node tube.js || exit 1
+node xeq.js  || exit 1
+node xhv.js  || exit 1
+node xla.js  || exit 1
+node xmr.js  || exit 1
+node xmv.js  || exit 1
+node xtnc.js || exit 1
+node zeph.js || exit 1
